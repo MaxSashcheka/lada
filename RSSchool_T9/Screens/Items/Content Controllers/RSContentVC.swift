55c80ea9942cbe5691fe70fe662946f28dfd1a7e
zm1ca <zmicier.fied@yandex.ru>
2021-08-01 13:35:42 +0300

    feat: add dismiss button to content views

---
+++ RSSchool_T9/Screens/Items/Content Controllers/RSContentVC.swift
@@ -0,0 +1,45 @@
+//
+// 📰 🐸 
+// Project: RSSchool_T9
+// 
+// Author: Źmicier Fiedčanka
+// On: 30.07.21
+// 
+// Copyright © 2021 RSSchool. All rights reserved.
+
+import UIKit
+
+class RSContentVC: UIViewController {
+    
+    let dismissButtton: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(systemName: "xmark"), for: .normal)
+        btn.tintColor = .white
+        btn.layer.cornerRadius = 20
+        btn.layer.borderWidth = 1
+        btn.layer.borderColor = UIColor.white.cgColor
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.addTarget(self, action: #selector(dismissController), for: .touchUpInside)
+        return btn
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layoutUI()
+    }
+    
+    private func layoutUI() {
+        view.addSubview(dismissButtton)
+        NSLayoutConstraint.activate([
+            dismissButtton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
+            dismissButtton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            dismissButtton.heightAnchor.constraint(equalToConstant: 40),
+            dismissButtton.widthAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    @objc private func dismissController() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
